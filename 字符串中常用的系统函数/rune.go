19c0c78284086c5c97e56a8cc9e5421bf01f0f5a
Geoffrey <1183681473@qq.com>
2020-07-06 11:29:50 +0800

    字符串遍历，同时处理有中文的问题，r:=[]rune(str)

--- 字符串中常用的系统函数/rune.go
+++ 字符串中常用的系统函数/rune.go
@@ -6,11 +6,10 @@ import (
 
 
 func main (){
-	str:="hello北京"
+	str:="hello 北京" 
 	// 字符串遍历，同时处理有中文的问题，r:=[]rune(str)
 	r:=[]rune(str)
 	for i:=0;i<len(r);i++ {
 		fmt.Printf("字符=%c\n",r[i])
 	}
-	
 }
