0a4aead7754650df6c4fa3dcafebfe99e3aacb3f
Geoffrey <1183681473@qq.com>
2020-07-07 11:26:19 +0800

    类型断言

--- go应用/类型断言/x.(T).go
+++ go应用/类型断言/x.(T).go
@@ -42,4 +42,24 @@ func main (){
 	3.T可以是非接口类型，如想断言合法，则T必须实现x的接口；
 	4.T也可以是接口，则x的动态类型也应该实现接口T；
 	5.类型断言如果非法，运行失败时会导致错误，为了避免这种错误，应该总是使用下面的方式来进行类型断言
+	6.接口变量的类型也可以使用一种特殊形式的 swtich 来检测:
+	
+			func classifier(items ...interface{}) {
+			for i, x := range items {
+				switch x.(type) {
+				case bool:
+					fmt.Printf("Param #%d is a bool\n", i)
+				case float64:
+					fmt.Printf("Param #%d is a float64\n", i)
+				case int, int64:
+					fmt.Printf("Param #%d is a int\n", i)
+				case nil:
+					fmt.Printf("Param #%d is a nil\n", i)
+				case string:
+					fmt.Printf("Param #%d is a string\n", i)
+				default:
+					fmt.Printf("Param #%d is unknown\n", i)
+				}
+			}
+		}
 */
