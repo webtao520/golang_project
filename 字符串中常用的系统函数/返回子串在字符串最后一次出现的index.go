e05d12d683acd7a6eb5aacb9750bd0930619181a
Geoffrey <1183681473@qq.com>
2020-07-07 09:59:47 +0800

    返回子串在字符串最后一次出现的index，如没有返回-1

---
+++ 字符串中常用的系统函数/返回子串在字符串最后一次出现的index.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main (){
+	index:=strings.Index("LAMP_go_lang","go")
+	// 返回子串在字符串最后一次出现的index，如没有返回-1
+	fmt.Printf("index=%v\n",index)
+}
+
+/*
+PS D:\goLang\github\golang_project\字符串中常用的系统函数> go run .\返回子串在字符串最后一次出现的index.go
+														index=5
+*/
