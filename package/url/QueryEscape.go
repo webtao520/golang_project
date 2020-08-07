f806b701ef054c3034a005ac9dc8f36fa989757a
Geoffrey <1183681473@qq.com>
2020-08-07 10:23:31 +0800

    转码

--- package/url/QueryEscape.go
+++ package/url/QueryEscape.go
@@ -20,4 +20,15 @@ import (
 func main() {
 	p := url.QueryEscape("/user?id=12")
 	fmt.Println(p) // %2Fuser%3Fid%3D12
+	str, err := url.QueryUnescape(p)
+	if err == nil {
+		fmt.Println(str)
+	}
 }
+
+/*
+=================== 运行结果 =======================
+	PS D:\goLang\github\golang_project\package\url> go run .\QueryEscape.go
+		%2Fuser%3Fid%3D12
+		/user?id=12
+*/
