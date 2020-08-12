fac0053a3e74a5172054198f3141e46ba94b01e8
Geoffrey <1183681473@qq.com>
2020-08-12 10:35:42 +0800

    string 转换为int （Atoi）

---
+++ package/strconv/string转换为int.go
@@ -0,0 +1,27 @@
+/**
+string转换为int：Atoi()
+
+func Atoi(s string) (int, error)
+由于string可能无法转换为int，所以这个函数有两个返回值：第一个返回值是转换成int的值，第二个返回值判断是否转换成功。
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// Atoi () string -> int
+	i, _ := strconv.Atoi("3")
+	fmt.Println(3 + i)
+	fmt.Printf("%T", i)
+}
+
+/*
+PS D:\goLang\github\golang_project\package\strconv> go run  string转换为int.go
+		6
+		int
+*/
