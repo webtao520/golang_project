907e5d1853eb29faf1fecc151bd98eb4c8e2ea00
Geoffrey <1183681473@qq.com>
2020-08-14 14:34:19 +0800

    停止定时器

--- package/time/NewTicker.go
+++ package/time/NewTicker.go
@@ -61,3 +61,33 @@ func main() {
 // 如果想停止定时器，只能让go程序自动结束
 
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.NewTimer(time.Second * 2)
+	ch := make(chan bool)
+	go func(t *time.Timer) {
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				fmt.Println("timer running....")
+				// 需要重置Reset 使t 重新开始计时
+				t.Reset(time.Second * 2)
+			case stop := <-ch:
+				if stop {
+					fmt.Println("time Stop")
+					return
+				}
+			}
+		}
+	}(t)
+	time.Sleep(10 * time.Second)
+	ch <- true
+	close(ch)
+	time.Sleep(1 * time.Second)
+}
