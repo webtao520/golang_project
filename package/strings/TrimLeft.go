9d89a5207d1880df8ddfe30568b1febfefdb1b0e
Geoffrey <1183681473@qq.com>
2020-10-14 14:50:42 +0800

    字符串去除

--- package/strings/TrimLeft.go
+++ package/strings/TrimLeft.go
@@ -1,11 +1,22 @@
-package main 
+/*
+func TrimLeft
+func TrimLeft(s string, cutset string) string
+返回将s前端所有cutset包含的utf-8码值都去掉的字符串。
+*/
+
+package main
 
 import (
 	"fmt"
 	"strings"
 )
 
-func main (){
-  str:="golang php"
-  fmt.Printf("%v",strings.TrimLeft(str,"go"))
+func main() {
+	str := "golang php"
+	fmt.Printf("%v", strings.TrimLeft(str, "go"))
 }
+
+/**
+PS D:\golang\github\golang_project\package\strings> go run TrimLeft.go
+lang php
+*/
