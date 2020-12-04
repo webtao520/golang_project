42589b8a073d055f12e10b5d9c1cf5c1dc7a0f75
Geoffrey <1183681473@qq.com>
2020-12-04 15:14:29 +0800

    Go语言初始化结构体的成员变量

---
+++ Go语言结构体/初始化结构体的成员变量/1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 打印消息类型，传入匿名结构体
+func printMsgType(msg *struct {
+	id   int
+	data string
+}) {
+	// 使用动词 %T 打印msg的类型
+	fmt.Printf("%T\n", msg)
+}
+
+func main() {
+	// 实例化 一个匿名结构体
+	msg := &struct { // 定义部门
+		id   int
+		data string
+	}{ //值初始化部门
+		1024,
+		"hello",
+	}
+	printMsgType(msg)
+}
