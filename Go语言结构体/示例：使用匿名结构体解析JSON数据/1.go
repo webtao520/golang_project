f55d250302e8b5147b8fd7183999f65e92a4a500
Geoffrey <1183681473@qq.com>
2020-12-08 10:11:58 +0800

    json 序列化

---
+++ Go语言结构体/示例：使用匿名结构体解析JSON数据/1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// 定义手机屏幕
+type Screen struct {
+	Size       float32 // 屏幕尺寸
+	ResX, ResY int     // 屏幕水平和垂直分辨率
+}
+
+//定义电池
+type Battery struct {
+	Capacity int // 容量
+}
+
+// 生成json数据
+func genJsonData() []byte {
+	// 完整数据结构
+	raw := &struct {
+		Screen
+		Battery
+		HasTouchID bool // 序列化时添加的字段：是否有指纹识别
+	}{
+		// 屏幕参数
+		Screen: Screen{
+			Size: 5.5,
+			ResX: 1920,
+			ResY: 1080,
+		},
+		// 电池参数
+		Battery: Battery{
+			2910,
+		},
+		// 是否有指纹识别
+		HasTouchID: true,
+	}
+	// 将数据序列化为json
+	jsonData, _ := json.Marshal(raw) // {"Size":5.5,"ResX":1920,"ResY":1080,"Capacity":2910,"HasTouchID":true}
+	return jsonData
+}
+
+func main() {
+	// 生成一段json数据
+	jsonData := genJsonData()
+	fmt.Println(string(jsonData))
+	//  只需要屏幕和指纹识别信息的结构和实例
+	screenAndTouch := struct {
+		Screen
+		HasTouchID bool
+	}{}
+	// 反序列化到 screenAndTouch
+	json.Unmarshal(jsonData, &screenAndTouch)
+	// 输出screenAndTouch的详细结构  %+v     打印结构体时，会添加字段名
+	fmt.Printf("%+v\n", screenAndTouch) // {Screen:{Size:5.5 ResX:1920 ResY:1080} HasTouchID:true}
+}
