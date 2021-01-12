4b33a3a25c314b0eb7629beda44d158426384813
Geoffrey <1183681473@qq.com>
2021-01-12 17:59:07 +0800

    goroutine

--- golang项目实战架构/day07/goroutine/main.go
+++ golang项目实战架构/day07/goroutine/main.go
@@ -11,6 +11,16 @@ func hello(i int){
 	fmt.Println("hello",i)
 }
 
+//程序启动之后会创建一个主 goroutine 去执行
 func main(){
-   
+     for  i:=0;i<1000;i++{
+		 // go hello(i) // 开启一个单独的goroutine去执行hello函数（任务）
+		  go func(i int) {
+			fmt.Println(i) // 用的是函数参数的那个i，不是外面的i
+		}(i)
+	 }
+
+	 fmt.Println("main")
+	 time.Sleep(time.Second)
+    // main函数结束了 由main函数启动的goroutine也都结束了
 }
