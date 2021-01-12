bfea10d72a57b9276ec2b988d9826a239851f45d
Geoffrey <1183681473@qq.com>
2021-01-12 10:53:58 +0800

    kafka

--- golang项目实战架构/day03/fuxi/main.go
+++ golang项目实战架构/day03/fuxi/main.go
@@ -7,5 +7,81 @@ import (
 // 复习
 
 func main(){
+	/*
+	var name string
+	name = "理想"
+	fmt.Println(name)
+	var ages [30]int // 声明一个变量ages， 他是[30]int类型
+	ages=[30]int{1,2,3,4}
+	fmt.Println(ages)
+	var ages2 = [...]int{1, 2, 3, 4, 5, 7, 8, 90}
+	fmt.Println(ages2)
+	var ages3 = [...]int{1:100,99:200}
+	fmt.Println(ages3)
+	// 二维数组
+	var a1 = [...][2]int{
+		 [2]int{1,2},
+		 [2]int{3,4},
+		 [2]int{5,6},
+	}
+	fmt.Println(a1) // [[1 2] [3 4] [5 6]]
+	// 多维数组 只有最外层可以使用...
+	
+	// 数组是值类型
+	x:=[3]int{1,2,3}
+	y:=x // 把x的值拷贝了一份给了y
+	y[0]=200 // 修改的是副本y,并不影响x
+	fmt.Println(y)
+
+	// 切片(slice)
+	var s1 []int 
+	fmt.Println(s1 == nil) // []  没有分配内存 == nil
+
+	s1 = []int{1,2,3}
+	fmt.Println(s1)
+
+	// make 初始化 分配内存
+	s2:=make([]bool,2,4)
+	fmt.Println(s2) // [false false]
+
+	s3:=make([]int,0,4)
+	fmt.Println(s3 == nil) // false 只要分配了内存 就不会是nil
+
+	s1 := []int{1, 2, 3} // [1 2 3]
+	s2 := s1
+	var s3 = make([]int, 3, 3)
+	copy(s3, s1) //  值copy
+	fmt.Println(s2) // [1 2 3]
+	s2[1] = 200
+	fmt.Println(s2) // [1 200 3]
+	fmt.Println(s1) // [1 200 3]
+	fmt.Println(s3) // ?[1 2 3]
+
+
+
+ var s1 []int  // nil 
+ s1 = make([]int,1)
+ s1[0]=100
+ fmt.Println(s1)
+ s1=append(s1, 1) // 自动初始化切片 
+ fmt.Println(s1)
+
+
+
+// 指针
+// go里面的指针只能读不能修改，不能修改指针变量指向的地址
+addr :="沙河"
+addrP:=&addr
+fmt.Println(addrP) // 内存地址
+fmt.Printf("%T\n", addrP) // 指针
+addrV := *addrP // 根据内存地址找值
+fmt.Println(addrV)
+*/
+
+// map
+
+
+
+
 	
 }
