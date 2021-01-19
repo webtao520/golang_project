fb7e09ac82e9dc3904f9e4574b9e00029ba12d5d
Geoffrey <1183681473@qq.com>
2021-01-19 17:08:44 +0800

    etcd get/put

--- ETCD/etcd_demo/main.go
+++ ETCD/etcd_demo/main.go
@@ -1,9 +1,50 @@
-package main 
+package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
 )
 
-func main(){
-	
+// etcd client put/get demo
+// use etcd/clientv3
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+
+	if err != nil {
+		// handle error
+		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("connect to etcd success")
+	defer cli.Close()
+
+	//  put
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	_, err = cli.Put(ctx, "test", "test01")
+	cancel()
+	if err != nil {
+		fmt.Printf("put to etcd failed,err: %v\n", err)
+		return
+	}
+
+	//get
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	resp, err := cli.Get(ctx, "test")
+	cancel()
+	if err != nil {
+		fmt.Printf("get from etcd failed, err:%v\n", err)
+		return
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s:%s\n", ev.Key, ev.Value)
+	}
 }
+
+//  客户端读取 命令 ：
+//  etcdctl.exe  --endpoints=http://127.0.0.1:2379 get test
